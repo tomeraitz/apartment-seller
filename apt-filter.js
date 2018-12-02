3dad3b3a9dd6e077ce2a08bc1786a23c0a3cb1ab
tomeraitz <tomeraitz1@gmail.com>
2018-12-02 17:30:03 +0200

    finished with the apartment

--- apt-filter.js
+++ apt-filter.js
@@ -2,7 +2,7 @@
 
 const must = (radio) => radio.filter(rb => rb.checked).length
 
-const findRelevantApts = function (address, minPrice, maxPrice, minRooms, maxRooms, immediate) {
+const findRelevantApts = function (address, minPrice, maxPrice, minRooms, maxRooms, immediate , park) {
     let relevantApts = apartments.filter(a =>
         a.address.toLowerCase().includes(address.toLowerCase() || "") &&
         a.price >= (minPrice || 0) &&
@@ -11,6 +11,18 @@ const findRelevantApts = function (address, minPrice, maxPrice, minRooms, maxRoo
         a.rooms <= (maxRooms || a.rooms)
     )
 
-    immediate = !immediate[0].checked
-    return immediate ? relevantApts : relevantApts.filter(a => (a.immediate)) 
+    immediate = immediate[0].checked 
+    park = park[0].checked
+    if(immediate && !park){
+        return relevantApts.filter(a => (a.immediate))
+    }
+    else if(park && !immediate){
+        return relevantApts.filter(a => (a.parking))
+    }
+    else if(immediate && park){
+        return relevantApts.filter(a => (a.parking && a.immediate))
+    }
+    else{
+        return relevantApts
+    }
 }
